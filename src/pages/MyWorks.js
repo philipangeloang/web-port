adbb73ce00faea393e0a9f4db28ae634c140324c
philipangeloang <philipangeloang1@gmail.com>
2023-05-23 01:12:32 +0800

    Added Sumida

--- src/pages/MyWorks.js
+++ src/pages/MyWorks.js
@@ -87,6 +87,32 @@ export default function MyWorks() {
                 />
               </div>
             </a>
+
+            <a href="https://sumida.netlify.app/">
+              <div className="h-[18rem] group/barangay bg-white rounded-lg relative overflow-hidden">
+                <div className="flex flex-col justify-center items-center text-white bg-gradient-to-br group from-main-blue/90 to-pink-600/70 absolute h-full w-full transition-opacity duration-300 cursor-pointer hover:opacity-0">
+                  <p className="font-poppins font-bold text-2xl | lg:text-3xl">
+                    Sumida
+                  </p>
+                  <p className="text-center text-xs | lg:text-base">
+                    Powered by OpenAI GPT-4
+                  </p>
+                </div>
+                <div className="group-hover/barangay:opacity-0 transition-opacity duration-300 absolute top-4 left-4 bg-gray-300/50 px-2 py-1 rounded-lg font-semibold text-xs text-white">
+                  Artificial Intelligence
+                </div>
+                <img
+                  className="rounded-lg z-10 hidden | sm:block | sm:h-full sm:object-contain"
+                  src="https://i.imgur.com/JvHjXua.jpg"
+                  alt="sumida"
+                />
+                <img
+                  className="rounded-lg z-10 block mx-auto | max-[390px]:h-full  | sm:hidden"
+                  src="https://i.imgur.com/JvHjXua.jpg"
+                  alt="sumida"
+                />
+              </div>
+            </a>
           </div>
         </div>
       </div>
